9fb2e0b4e908555cff254bc58a19c11e4afe0914
cxkeeley <cxkeeley@gmail.com>
2022-06-04 20:57:37 +0700

    commit

--- src/Product.js
+++ src/Product.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-import './Product.css'
+import './Product.css';
 
-const Product = () => {
+const Product = ({ id, title, image, price, rating }) => {
   return (
-    <div className="product">
-      <div className="product__info">
-        <p>The lean startup</p>
-        
+    <div className='product'>
+      <div className='product__info'>
+        <p>{title}</p>
+        <p className='product__price'>
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className='product__rating'>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
       </div>
-    </div>
-  )
-}
 
-export default Product
+      <img src={image} alt='' />
+
+      <button>Add to Basket</button>
+    </div>
+  );
+};
+
+export default Product;
