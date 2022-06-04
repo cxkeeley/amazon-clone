f0c1e9fa1dcba26f56a0b8df0abc19169eb66143
cxkeeley <cxkeeley@gmail.com>
2022-06-04 17:06:07 +0700

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className='home__container'>
+        <img
+          src='https://m.media-amazon.com/images/I/51PG7WjcL7L._SX1500_.jpg'
+          alt=''
+          className='home__image'
+        />
+
+        <div className='home__row'>
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className='home__row'>
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className='home__row'>
+          {/* Product */}
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Product.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Product.css'
+
+const Product = () => {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Product
