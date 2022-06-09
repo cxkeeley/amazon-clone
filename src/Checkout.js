22b7193658241789d7f751c075fd16d233b029c9
cxkeeley <cxkeeley@gmail.com>
2022-06-09 20:11:50 +0700

    commit

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 
 const Checkout = () => {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className='checkout'>
       <div className='checkout__left'>
@@ -12,14 +16,18 @@ const Checkout = () => {
           alt=''
         />
         <div>
+          <h2 className='checkout__title'>Your Shopping Basket</h2>
 
-        <h2 className="checkout__title">Your Shopping Basket</h2>
+          {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
 
-        {/* BasketItem */}
-        {/* BasketItem */}
-        {/* BasketItem */}
-        {/* BasketItem */}
-        {/* BasketItem */}
         </div>
       </div>
       <div className='checkout__right'>
--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,10 @@
 export const initialState = {
-  basket: [],
+  basket: []
 };
 
 // Selector
-export const getBasketTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0);
+export const getBasketTotal = basket =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
   console.log(action);
@@ -14,8 +15,23 @@ const reducer = (state, action) => {
         basket: [...state.basket, action.item]
       };
 
-      default:
-        return state;
+    case 'REMOVE_FROM_BASKET':
+      const index = state.basket.findIndex((basketItem) => basketItem.id === action.id);
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(`Can't remove product (id: ${action.id}) as its not in basket!`);
+      }
+
+      return {
+        ...state,
+        basket: newBasket
+      };
+
+    default:
+      return state;
   }
 };
 
